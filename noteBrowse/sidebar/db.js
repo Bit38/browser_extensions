66bc63c0cc8388a35e2699a6b9ab6899aaedfa2c
Bit38 <119065583+Bit38@users.noreply.github.com>
2025-06-12 20:47:03 +0200

    Added indexedDB warapper and ability to list existing notes

---
+++ noteBrowse/sidebar/db.js
@@ -0,0 +1,90 @@
+class DBIndexed {
+  #db;
+
+  constructor(db) {
+    this.#db = db;
+  }
+
+  // TODO: merge into load()
+  static _open() {
+    return new Promise((resolve, reject) => {
+      const dbReq = window.indexedDB.open("noteBrowse_notes", 1);
+      dbReq.onerror = (err) => {
+        console.dir(err);
+        reject("Failed to open a db");
+      };
+
+      dbReq.onsuccess = (ev) => {
+        resolve(ev.target.result);
+      };
+
+      dbReq.onupgradeneeded = (ev) => {
+        const db = ev.target.result;
+        db.onerror = (ev) => {
+          reject("Failed to open db");
+        };
+
+        const notes = db.createObjectStore("notes", {
+          keyPath: "id",
+          autoIncrement: true,
+        });
+
+        notes.createIndex("title", "title", { unique: false });
+        notes.createIndex("content", "content", { unique: false });
+        notes.createIndex("created", "created", { unique: false });
+      };
+    });
+  }
+
+  // Loads db
+  static async load() {
+    const db = await DBIndexed._open();
+
+    return new DBIndexed(db);
+  }
+
+  getNotes() {
+    const os = this.#db.transaction(["notes"], "readonly").objectStore("notes");
+
+    return new Promise((resolve, reject) => {
+      const dbReq = os.getAll();
+      dbReq.onsuccess = (ev) => {
+        resolve(dbReq.result);
+      };
+      dbReq.onerror = (ev) => {
+        reject(ev.error);
+      };
+    });
+  }
+
+  addNote(title, content) {
+    const os = this.#db
+      .transaction(["notes"], "readwrite")
+      .objectStore("notes");
+
+    const dbReq = os.put({
+      title: title,
+      content: content,
+      created: new Date(),
+    });
+
+    return new Promise((resolve, reject) => {
+      dbReq.onsuccess = () => {
+        resolve();
+      };
+
+      dbReq.onerror = (ev) => {
+        reject(ev.error);
+      };
+    });
+  }
+
+  close() {
+    this.#db.close();
+  }
+}
+
+export async function loadDB() {
+  const loadedDB = await DBIndexed.load();
+  return loadedDB;
+}
--- noteBrowse/sidebar/sidebar.js
+++ noteBrowse/sidebar/sidebar.js
@@ -1,9 +1,19 @@
+import { loadDB } from "./db.js";
+
 const button_new = document.getElementById("btn-new");
 const button_save = document.getElementById("btn-save");
 const button_delete = document.getElementById("btn-del");
 const input_title = document.getElementById("note-title");
 const input_content = document.getElementById("note-content");
+const note_list = document.querySelector(".note-list");
 const panel2 = document.querySelector(".panel2");
+
+const db = await loadDB();
+window.ddb = db;
+
+window.addEventListener("beforeunload", () => {
+  db.close();
+});
 
 button_new.onclick = () => {
   input_title.value = "";
@@ -28,20 +38,25 @@ button_save.onclick = () => {
   panel2.classList.remove("show");
 };
 
-function update_listeners_li() {
-  const lis = document.querySelectorAll("li");
+async function reload_note_list() {
+  const ul = document.querySelector("ul.note-list");
+  const notes = await db.getNotes();
 
-  for (const el of lis) {
-    el.onclick = () => {
-      // TODO: recognise which note is clicked (id?)
-      console.log("note clicked");
+  const elements = notes.map((note) => {
+    const root = document.createElement("li");
+    const title = document.createElement("h2");
+    const dateText = document.createElement("h4");
+    const d = note.created;
 
-      input_title.value = "Sample title";
-      input_content.value = "Sample content";
+    title.textContent = note.title;
+    dateText.textContent = `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, "0")}-${String(d.getDate()).padStart(2, "0")}`;
 
-      panel2.classList.add("show");
-    };
-  }
+    root.append(title, dateText, document.createElement("span"));
+
+    return root;
+  });
+
+  note_list.replaceChildren(...elements);
 }
 
-update_listeners_li();
+await reload_note_list();
